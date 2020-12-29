91a0611dfae86f077c66165302a30055b9ccd208
simei-yeh <simei.yeh@gmail.com>
2020-12-28 21:36:12 -0800

    Updated schema. working on market data retrieval

--- app/repository/api_ibkr_retrieveMarketData.py
+++ app/repository/api_ibkr_retrieveMarketData.py
@@ -6,6 +6,8 @@ import json
 
 headers = { 'content-type': 'application/json;charset=utf-8' }
 
+timeOpts = [{'bar': '1d', 'period': '1y'}, {'bar': '1w', 'period': '5y'}, {'bar': '30min', 'period': '1m'}, {'bar':'15min','period':'1w'}]
+
 def getConId():
     # create a formatted string of the Python JSON object
     endpoint="https://localhost:8080/v1/api/iserver/marketdata/history"
@@ -14,17 +16,16 @@ def getConId():
     conids = connect(query0, "fetch")
 
     for x in conids:
-      payload = { 'conids': x[0] }
-      print(payload)
-    #   r = requests.get(endpoint, headers=headers, params=payload, verify=False).json()
+      for y in timeOpts:
+        payload = dict({ 'conid': x[0] }, **y)
+        print(payload)
+        r = requests.get(endpoint, headers=headers, params=payload, verify=False).json()
 
-    #     data = ({key: r[0][key] for key in r[0].keys() & { 'conid','symbol' }})
-    #     data['companyname']=r[0]['companyName']
-    #     data['opt']=[r[0]['opt']]
-    #     formatted = json.dumps(data)
-    #     query = "INSERT INTO vol.conid SELECT * from json_populate_record(NULL::vol.conid,'{}') \
-    #      ON CONFLICT (conid) DO UPDATE SET(opt) = \
-    #           (SELECT opt FROM json_populate_record (NULL::vol.conid,'{}'))".format(formatted,formatted)
+        data = ({key: r[0][key] for key in r[0].keys() & { 'conid','symbol' }})
+        data['opt']=[r[0]['opt']]
+        formatted = json.dumps(data)
+        query = "INSERT INTO vol.stocks SELECT * from json_populate_record(NULL::vol.stocks,'{}') ON CONFLICT (stockCode) DO UPDATE SET(opt) = \
+          (SELECT opt FROM json_populate_record (NULL::vol.stocks,'{}'))".format(formatted,formatted)
 
     #     results = connect(query,"insert")
     #     print(results, '', x)
