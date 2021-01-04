8555f9a42463e7380ce81d9b68af48266e51b048
simei-yeh <simei.yeh@gmail.com>
2021-01-03 18:51:39 -0800

    updated graphOptions styling

--- app/react-app/src/GraphOptions.jsx
+++ app/react-app/src/GraphOptions.jsx
@@ -4,10 +4,21 @@ import './GraphOptions.css'
 const GraphOptions = () => {
   const timeOptions = ['5min', '15min', '30min', 'daily', 'weekly']
 
+  const[timeValue, setValue] = useState({option: ''})
+
+  const handleTimeClick = (event) => {
+    event.persist();
+    event.preventDefault();
+    setValue((timeValue) => ({
+      ...timeValue,
+      option: event.target.value,
+    }));
+    console.log('clicked ', timeValue)
+  }
 
   return (
     <div className="options-bar-wrapper">
-      {timeOptions.map(d => <button key={d}>{d}</button>)}
+      {timeOptions.map(d => <button className="timeButton" key={d} value={d} onClick={handleTimeClick}>{d}</button>)}
     </div>
   );
 }
