ee1f41c82b73400ce916d2a35f12f533c08dadc4
simei-yeh <simei.yeh@gmail.com>
2021-01-03 19:52:54 -0800

    Updated graphOptions to useEffect

--- app/react-app/src/GraphOptions.jsx
+++ app/react-app/src/GraphOptions.jsx
@@ -1,19 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './GraphOptions.css'
 
 const GraphOptions = ({callback}) => {
   const timeOptions = ['5min', '15min', '30min', 'daily', 'weekly']
 
+  const[timeSelected, setTimeSelected] = useState(false)
   const[timeValue, setValue] = useState({option: '30min'})
 
   const handleTimeClick = (event) => {
     event.preventDefault();
     setValue((timeValue) => ({
+      ...timeValue,
       option: event.target.value,
     }));
-    console.log('clicked ', timeValue)
-    callback(undefined, timeValue.option)
+    setTimeSelected(true);
   }
+
+  useEffect(() => {
+    if (timeSelected) {
+      console.log('clicked ', timeValue)
+      callback(undefined, timeValue.option)
+      setTimeSelected(false)
+    }
+  })
 
   return (
     <div className="options-bar-wrapper">
